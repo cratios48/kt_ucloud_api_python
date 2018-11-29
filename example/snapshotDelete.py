0456cc4124e5ce6d61dff1b7fb0a79299e896b20
Jeong JH <cratios48@users.noreply.github.com>
2018-11-29 13:47:46 +0900

    파일 관련 예제 수정

--- example/snapshotDelete.py
+++ example/snapshotDelete.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python3
 
 from time import sleep
+import os
+import json
 import server
 
-zone = ''
-apikey = ''
-secretkey = ''
+with open(os.path.abspath(os.path.dirname(__file__)) + './key.json', 'r') as f:
+    keyInfo = json.load(f)
+
+zone = keyInfo['key']['zone']
+apikey = keyInfo['key']['apikey']
+secretkey = keyInfo['key']['secretkey']
 
 targetSs = server.Snapshot(zone, apikey, secretkey)
 targetVol = server.Volume(zone, apikey, secretkey)
