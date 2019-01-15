68993f1e5a0641df145fe66edf509d0c81ccb0cb
Jeong JH <cratios48@users.noreply.github.com>
2019-01-15 16:46:30 +0900

    디렉토리 구조 및 예제 변경

--- example/snapshotDelete.py
+++ example/snapshotDelete.py
@@ -5,7 +5,7 @@ import os
 import json
 import server
 
-with open(os.path.abspath(os.path.dirname(__file__)) + './key.json', 'r') as f:
+with open(os.path.abspath(os.path.dirname(__file__)) + '/key.json', 'r') as f:
     keyInfo = json.load(f)
 
 zone = keyInfo['key']['zone']
--- example/volumeList.py
+++ example/volumeList.py
@@ -4,13 +4,13 @@ import os
 import json
 import server
 
-with open(os.path.abspath(os.path.dirname(__file__)) + '/key.json', 'r') as f:
+with open(os.path.abspath(os.path.dirname(__file__)) + './key.json', 'r') as f:
     keyInfo = json.load(f)
 
 zone = keyInfo['key']['zone']
 apikey = keyInfo['key']['apikey']
 secretkey = keyInfo['key']['secretkey']
 
-targetSs = server.Snapshot(zone, apikey, secretkey)
+targetVol = server.Volume(zone, apikey, secretkey)
 
-print(targetSs.lists())
+print(targetVol.lists())
